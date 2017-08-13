e340b003b85c91a8ad9931dbef1cf97c899d2a5c
jalexanderg8 <alexander8z@hotmail.com>
2017-08-13 10:47:05 -0500

    Proyecto base Con fragment contenedor

--- app/src/main/java/com/example/foxconntech/egresoft/activitys/Principal.java
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Principal.java
@@ -1,16 +1,30 @@
 package com.example.foxconntech.egresoft.activitys;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.foxconntech.egresoft.R;
+import com.example.foxconntech.egresoft.fragments.FragmentContenedor;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements FragmentContenedor.OnFragmentInteractionListener{
 
+    Fragment miFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        miFragment=new FragmentContenedor();
+        getSupportFragmentManager().beginTransaction().replace(R.id.principal,miFragment).commit();
+
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
