8d009d4f2bc18984efad971fe0bcaf161929931f
HENAOMIGUEL <miguelmahh6@gmail.com>
2017-08-16 11:15:26 -0500

    bottomNavigationBar sin navegacion

--- app/src/main/java/com/example/foxconntech/egresoft/activitys/Principal.java
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Principal.java
@@ -1,30 +1,18 @@
 package com.example.foxconntech.egresoft.activitys;
 
-import android.net.Uri;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.foxconntech.egresoft.R;
-import com.example.foxconntech.egresoft.fragments.FragmentContenedor;
-import com.example.foxconntech.egresoft.fragments.FragmentLogin;
 
-    public class Principal extends AppCompatActivity implements FragmentContenedor.OnFragmentInteractionListener,
-    FragmentLogin.OnFragmentInteractionListener{
+public class Principal extends AppCompatActivity {
 
-    Fragment miFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-
-
-
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
-    }
 }
