a752efa5ae3bc7e778ed1a29da52ea120edc04f5
Foxconn Tech <alexander8z@hotmail.com>
2017-08-30 08:44:40 -0500

    Registro activity

--- app/src/main/java/com/example/foxconntech/egresoft/activitys/Login.java
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Login.java
@@ -1,16 +1,37 @@
 package com.example.foxconntech.egresoft.activitys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.foxconntech.egresoft.R;
 
 public class Login extends AppCompatActivity {
 
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+           case  R.id.btnIngEgresado:
+           i=new Intent(this,Principal.class);
+            startActivity(i);
+               break;
+            case  R.id.btnIngUsuario:
+                i=new Intent(this,Principal.class);
+                startActivity(i);
+            case R.id.lblRegistrarse:
+
+
+        }
     }
 }
