4f67c2d84e1e991dc91a8fe15d4b919baeb77172
Foxconn Tech <alexander8z@hotmail.com>
2017-08-30 21:14:29 -0500

    Registro activity

--- app/src/main/java/com/example/foxconntech/egresoft/activitys/Registro.java
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Registro.java
@@ -2,14 +2,25 @@ package com.example.foxconntech.egresoft.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.foxconntech.egresoft.R;
 
 public class Registro extends AppCompatActivity {
 
+    Spinner comboTitulacion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+      comboTitulacion=(Spinner)findViewById(R.id.idSinnerTitulacion);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.combo_titulacion,
+                android.R.layout.simple_spinner_item);
+
+        comboTitulacion.setAdapter(adapter);
     }
 }
