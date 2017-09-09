6956be941675438d425343bfbf40ae64d9233813
HENAOMIGUEL <miguelmahh6@gmail.com>
2017-09-09 14:02:24 -0500

    no me vayan a tocar nada de lo mio

---
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Splash.java
@@ -0,0 +1,163 @@
+package com.example.foxconntech.egresoft.activitys;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foxconntech.egresoft.R;
+import com.example.foxconntech.egresoft.fragments.fragments_splash.cuatro;
+import com.example.foxconntech.egresoft.fragments.fragments_splash.dos;
+import com.example.foxconntech.egresoft.fragments.fragments_splash.tres;
+import com.example.foxconntech.egresoft.fragments.fragments_splash.uno;
+
+public class Splash extends AppCompatActivity implements uno.OnFragmentInteractionListener,dos.OnFragmentInteractionListener
+,tres.OnFragmentInteractionListener,cuatro.OnFragmentInteractionListener{
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (Principal.info){
+                        finish();
+                    }else{
+                        finish();
+                        Intent intento = new Intent(Splash.this, Login.class);
+                        startActivity(intento);
+
+                    }
+
+                }
+            }
+        }).start();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Fragment newInstance(int sectionNumber) {
+            Fragment fragment=null;
+
+            switch (sectionNumber){
+                case 1:fragment=new uno();
+                    break;
+
+                case 2:fragment=new dos();
+                    break;
+
+                case 3:fragment=new tres();
+                    break;
+
+                case 4:fragment=new cuatro();
+                    break;
+            }
+
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_splash, container, false);
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+                case 3:
+                    return "SECTION 4";
+            }
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foxconntech/egresoft/fragments/FragmentConvenio.java
@@ -0,0 +1,142 @@
+package com.example.foxconntech.egresoft.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foxconntech.egresoft.R;
+import com.example.foxconntech.egresoft.adaptadores.Adaptador_Convenios;
+import com.example.foxconntech.egresoft.vo.Convenio_vo;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentConvenio.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentConvenio#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentConvenio extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    RecyclerView Convenios_Recycler;
+    ArrayList<Convenio_vo> ListaConvenios;
+
+    public FragmentConvenio() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentConvenio.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentConvenio newInstance(String param1, String param2) {
+        FragmentConvenio fragment = new FragmentConvenio();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+View vista=inflater.inflate(R.layout.fragment_fragment_convenio, container, false);
+
+Convenios_Recycler=vista.findViewById(R.id.recycler_Convenios);
+
+        llenarLista();
+
+       Convenios_Recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        Adaptador_Convenios miAdaptador=new Adaptador_Convenios(ListaConvenios);
+        Convenios_Recycler.setAdapter(miAdaptador);
+
+        return vista;
+    }
+
+    private void llenarLista() {
+        ListaConvenios=new ArrayList<>();
+        ListaConvenios.add(new Convenio_vo("Universidad del quindio","homologa 5 semestres","pepito arturo sandoval 311544888",R.drawable.convenio1));
+        ListaConvenios.add(new Convenio_vo("EAM","homologa 5 semestres","pepito arturo sandoval 311544888",R.drawable.tic));
+        ListaConvenios.add(new Convenio_vo("Universidad la grancolombia","homologa 5 semestres","pepito arturo sandoval 311544888",R.drawable.navidad));
+        ListaConvenios.add(new Convenio_vo("Remington","homologa 5 semestres","2pepito arturo sandoval 311544888",R.drawable.logo_sena_verde));
+
+
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
