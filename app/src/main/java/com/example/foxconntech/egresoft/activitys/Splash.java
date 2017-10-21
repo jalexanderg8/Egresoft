30a29ec429db3f82782f54694243422000e3cb7d
alexander <alexander8z@hotmail.com>
2017-10-20 22:06:05 -0500

    Splah funcionando con animacion 3 segundos

--- app/src/main/java/com/example/foxconntech/egresoft/activitys/Splash.java
+++ app/src/main/java/com/example/foxconntech/egresoft/activitys/Splash.java
@@ -1,163 +1,43 @@
 package com.example.foxconntech.egresoft.activitys;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.foxconntech.egresoft.R;
-import com.example.foxconntech.egresoft.fragments.fragments_splash.cuatro;
-import com.example.foxconntech.egresoft.fragments.fragments_splash.dos;
-import com.example.foxconntech.egresoft.fragments.fragments_splash.tres;
-import com.example.foxconntech.egresoft.fragments.fragments_splash.uno;
 
-public class Splash extends AppCompatActivity implements uno.OnFragmentInteractionListener,dos.OnFragmentInteractionListener
-,tres.OnFragmentInteractionListener,cuatro.OnFragmentInteractionListener{
+public class Splash extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        imageView=(ImageView)findViewById(R.id.imagenSplash);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.rotacion);
+        animation.setFillAfter(true);
+        imageView.startAnimation(animation);
 
 
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (Principal.info){
-                        finish();
-                    }else{
-                        finish();
-                        Intent intento = new Intent(Splash.this, Login.class);
-                        startActivity(intento);
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Splash.this.startActivity(new Intent(Splash.this,Slider.class));
+              Splash.this.finish();
+          }
+      },3000);
 
-                    }
-
-                }
-            }
-        }).start();
-
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Fragment newInstance(int sectionNumber) {
-            Fragment fragment=null;
-
-            switch (sectionNumber){
-                case 1:fragment=new uno();
-                    break;
-
-                case 2:fragment=new dos();
-                    break;
-
-                case 3:fragment=new tres();
-                    break;
-
-                case 4:fragment=new cuatro();
-                    break;
-            }
-
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_splash, container, false);
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-                case 3:
-                    return "SECTION 4";
-            }
-            return null;
-        }
     }
 }
